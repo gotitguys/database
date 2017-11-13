1d6181074d8bba869c4a983b5d07d46c7ea11938
Tyrannosarous-Esteban <elopez60@csub.edu>
2017-11-12 23:34:07 -0800

    status

---
+++ status.sql
@@ -0,0 +1,30 @@
+INSERT INTO status(S_id, State)
+  VALUES('72253846','placed');
+INSERT INTO status(S_id, State)
+  VALUES('72215368','placed');
+INSERT INTO status(S_id, State)
+  VALUES('71965222','processed');
+INSERT INTO status(S_id, State)
+  VALUES('62514253','shipped');
+INSERT INTO status(S_id, State)
+  VALUES('92551366','processed');
+INSERT INTO status(S_id, State)
+  VALUES('75226415','shipped');
+INSERT INTO status(S_id, State)
+  VALUES('85539751','placed');
+INSERT INTO status(S_id, State)
+  VALUES('63357910','processed');
+INSERT INTO status(S_id, State)
+  VALUES('63462530','placed');
+INSERT INTO status(S_id, State)
+  VALUES('72963514','placed');
+INSERT INTO status(S_id, State)
+  VALUES('90852369','processed');
+INSERT INTO status(S_id, State)
+  VALUES('75362547','shipped');
+INSERT INTO status(S_id, State)
+  VALUES('64008353','placed');
+INSERT INTO status(S_id, State)
+  VALUES('98632536','placed');
+INSERT INTO status(S_id, State)
+  VALUES('64253611','placed');
