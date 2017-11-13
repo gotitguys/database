0db3932fdea86e1052123935251bdb7a355c0af7
Tyrannosarous-Esteban <elopez60@csub.edu>
2017-11-12 23:56:17 -0800

    updates started but not done

---
+++ updates.sql
@@ -0,0 +1,8 @@
+INSERT INTO (Time, S_id, order_num) 
+  VALUES('9:14', '72253846', '100000');
+INSERT INTO (Time, S_id, order_num) 
+  VALUES('9:14', '72215368', '100001');
+INSERT INTO (Time, S_id, order_num) 
+  VALUES('9:14', '71965222', '1000002');
+INSERT INTO (Time, S_id, order_num) 
+  VALUES('10:30', '72253846', '100003');
