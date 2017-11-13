fc6540da8f27d85cc1d0bb9f0d8fa3819679ed72
Tyrannosarous-Esteban <elopez60@csub.edu>
2017-11-12 23:22:05 -0800

    customer

---
+++ customer.sql
@@ -0,0 +1,60 @@
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000000', 'Luis', 'R', 'Breeland', 'Password21',
+'Luis.breeland@gmail.com', '7188319952', '1000000', '4342562220704736' );
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000001', 'Justin', 'R', 'Zuniga', 'QwerTy@7',
+'zuniga11@aol.com', '7184726328', '1000001', '4266841530761907' );
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000002', 'Brian', 'S', 'Wood', 'GuesT@1',
+'wood.brian93@gmail.com', '5188340823', '1000002', '5152500002964726' );
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000003', 'Santiago', 'S', 'Vasquez', 'P@ssword38',
+'Vasquez.s@icloud.com', '8436026395', '1000003', '4342562220738279' );
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000004', 'Erik', 'F', 'Kline', 'GueSt!5',
+'fallcloud@yahoo.com', '8725416658', '1000004', '4266841531066297' );
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000005', 'Chris', 'J', 'Wood', '!QWertY2#',
+'wood4chris@gmail.com', '5596215398', '1000005', '4342562220695736' );
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000006', 'Emma', NULL, 'Brown', 'Password!9',
+'a.brown1994@aol.com', '8728379622', '1000006', '5152500004725236' );
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000007', 'Charles', 'T', 'Johnson', 'qwERTY@1',
+'chjohnson12@gmail.com', '8438345236', '1000007', '4266841532046385' );
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000008', 'Darlene', 'B', 'Hutcherson', 'guE5t@',
+'Tiahutherson@icloud.com', '8722276363', '1000008', '5152500010395423' );
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000009', 'Marnel', 'L', 'Shirley', 'Password@4',
+'marnel07shirley@yahoo.com', '7184721680', '1000009', '4342562220716439' );
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000010', 'Joseph', NULL, 'Krigbam', '!qW#RtY!',
+'krigbam.j@gmail.com', '7184276235', '1000010', '4266841531143975' );
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000011', 'Dorothy', 'E', 'Krigbam', 'Gue$T1',
+'krigbam.d6@yahoo.com', '8434184661', '1000011', '5152500007552469' );
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000012', 'Brandon', 'G', 'Allen', 'P@ssword10',
+'brandon.allen11@icloud.com', '661-325-6637', '1000012', '5152500015248087' );
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000013', 'Deshante', 'P', 'King', 'K!ng$1@',
+'DeKingsword@gmail.om', '7186425279', '1000013', '4266841530019763' );
+INSERT INTO customer(Customer_id, Fname, Middle_init, Lname, Pword, Email,
+phone, Order_num, Card_num)
+  VALUES('2000014', 'Jesue', 'F', 'Sandoval', 'P@ssw0rd2#',
+'SandovalisJesus@aol.com', '8432019573', '1000014', '4342562220705842' );
