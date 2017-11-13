5c14f0924ead2842d3cad6a2b7f2e3fb4ae7726b
Tyrannosarous-Esteban <elopez60@csub.edu>
2017-11-12 21:46:14 -0800

    orders

---
+++ order.sql
@@ -0,0 +1,30 @@
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000456','09/17/17', '7:32','Null', '4342562220704736', '1037951');
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000457','09/17/17', '9:14','Null', '4266841530761907', '1638762');
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000458','09/17/17', '11:57','Null', '5152500002964276', '1147263');
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000459','09/17/17', '15:27','Null', '4342562220738279', '1325478');
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000460','09/17/17', '19:14','Null', '4266841531066297', '1752386');
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000461','09/17/17', '22:48','7.975', '4342562220695736', '1967384');
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000462','09/18/17', '2:36','Null', '5152500004725236', '1187113');
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000463','09/18/17', '5:07','Null', '4266841532046385', '1741581');
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000464','09/18/17', '8:39','Null', '5152500010395423', '1325641');
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000465','09/18/17', '11:21','Null', '4342562220716439', '1172415');
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000466','09/18/17', '15:52','Null', '4266841531143975', '1725134');
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000467','09/18/17', '19:04','Null', '5125500007552469', '1936254');
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000468','09/18/17', '23:19','7.5', '5125500015248087', '1143215');
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000469','09/19/17', '4:51','Null', '4266841530019763', '1741521');
+INSERT INTO orders (Order_num, Datee, Time, Tax_rate, Card_num, cutomer_id)
+  VALUES('10000470','09/19/17', '7:35','Null', '4342562220705842', '1153624');
