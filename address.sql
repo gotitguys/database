af8d3a9ddbf245c935e321cd6fceff9caa13246c
abutanda <abutanda@csub.edu>
2017-11-12 17:11:15 -0800

    Add files via upload
    address data

---
+++ address.sql
@@ -0,0 +1,30 @@
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('4304',	'5th ave', '10019', 'New York', 'NY', '1000000', 'home','1037951');																		
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('152',	'K st', '11434', 'Queens', 'NY', '1000001', 'home','1638762');																		
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('805', 'Snapdragon ln', '12020', 'Ballston Spa', 'NY', '1000002', 'work','1147263');																		
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('3812',	'Peachwood ct', '29401', 'Charleston', 'SC', '1000003', 'home','1325478');																		
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('4009',	'Gwendolyn rd', '60107', 'Chicago', 'IL', '1000004', 'home','1752386');																		
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('17',	'Main st', '93701', 'Fresno', 'CA', '1000005', 'home','1967384');																		
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('85',	'32nd st', '60106', 'Chicago', 'IL', '1000006', 'home','1187113');																		
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('952',	'Lake st', '29405', 'Charleston', 'SC', '1000007', 'work','1741581');																		
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('1701',	'Soranno ave', '60014', 'Chicago', 'IL', '1000008', 'home','1325641');																		
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('330',	'Park pl', '11214', 'Brooklyn', 'NY', '1000009', 'work','11724150');																		
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('3801', '17th ave', '10025', 'New York', 'NY', '1000010', 'work','1725134');																		
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('640', 'Robinwood st', '29406', 'Charleston', 'SC', '1000011', 'home','1936254');																		
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('3654',	'Merigold way', '93309', 'Bakersfield', 'CA', '1000012', 'home','1143215');																		
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('297',	'Mckinley ave', '112070', 'Brooklyn', 'NY', '1000013', 'work','1741521');																		
+INSERT INTO address (street_no, Street, zip, city, state, order_num, type,  Customer_id)  
+	VALUES('1403',	'Main st', '10019', 'Goose Creek', 'SC', '1000014', 'home','1153624');																		
