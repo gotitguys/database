6fab2c01889cf2894fca632000c0bbeee37af9c9
abutanda <abutanda@csub.edu>
2017-11-27 14:42:27 -0800

    Add files via upload

--- products.sql
+++ products.sql
@@ -7,24 +7,24 @@ INSERT into Products(P_id, Category, P_name, S_price, P_price, D_id)
 INSERT into Products(P_id, Category, P_name, S_price, P_price, D_id)
 	VALUES('359', 'Marvel', 'Cable& Deadpool #1-50', '99.99','40.00','1001');
 INSERT into Products(P_id, Category, P_name, S_price, P_price, D_id)
-	VALUES(); 
+	VALUES('451','DC','All-Flash Vol 1', '45.79','18.32','1001'); 
 INSERT into Products(P_id, Category, P_name, S_price, P_price, D_id)
-	VALUES(); 
+	VALUES('368','Marvel','Captain America Vol 7 #1-4', '19.99', '8.00','1001'); 
 INSERT into Products(P_id, Category, P_name, S_price, P_price, D_id)
-	VALUES(); 
+	VALUES('690','IDW','Godzilla:Kingdom of Monster', '24.99', '9.80', '1001'); 
 INSERT into Products(P_id, Category, P_name, S_price, P_price, D_id)
-	VALUES(); 
+	VALUES('539', 'DC', 'The All-New Batman: The Brave and the Bold', '19.99', '8.00','1001'); 
 INSERT into Products(P_id, Category, P_name, S_price, P_price, D_id)
-	VALUES(); 
+	VALUES('148','Marvel','Moon Knight Vol 2 #1-6', '19.99', '8.00','1001');  
 INSERT into Products(P_id, Category, P_name, S_price, P_price, D_id)
-	VALUES(); 
+	VALUES('421','IDW', 'The A-Team: War Stories', '29.99','12.00','1001'); 
 INSERT into Products(P_id, Category, P_name, S_price, P_price, D_id)
-	VALUES(); 
+	VALUES('369', 'DC', 'The All-New Batman:The Brave and the Bold #1-20','199.99','80.00', '1001'); 
 INSERT into Products(P_id, Category, P_name, S_price, P_price, D_id)
-	VALUES(); 
+	VALUES('221', 'DC', 'All Star Batman #1', '24.99','9.80', '1001'); 
 INSERT into Products(P_id, Category, P_name, S_price, P_price, D_id)
-	VALUES(); 
+	VALUES('123', 'Marvel', 'Fantastic Four Vol 4 #1-7', '19.99','8.00','1001'); 
 INSERT into Products(P_id, Category, P_name, S_price, P_price, D_id)
-	VALUES(); 
+	VALUES('145','DC', 'Arrow Special Edition', '9.99','4.00','1001'); 
 INSERT into Products(P_id, Category, P_name, S_price, P_price, D_id)
-	VALUES(); 
+	VALUES('853', 'Marvel', 'Captain America Vol 7 #5-7','17.99','7.20','1001'); 
