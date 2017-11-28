c91cec1b4e95de6c3430091f4bd92dd306aa4022
Tyrannosarous-Esteban <elopez60@csub.edu>
2017-11-28 14:42:27 -0800

    recieves

---
+++ recieves.sql
@@ -0,0 +1,41 @@
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('1',	'09/23/17', '7.99', '30', '458');
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('3',	'09/23/17', '4.00', '30', '139');
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('1',	'09/23/17', '7.20', '30', '275');
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('1',	'09/23/17', '40.00', '31', '359');
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('1',	'09/23/17', '18.32', '31', '451');
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('2',	'09/23/17', '8.00', '31', '368');
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('1',	'09/23/17', '9.80', '31', '690');
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('2',	'09/23/17', '8.00', '32', '539');
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('1',	'09/24/17', '8.00', '32', '148');
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('1',	'09/24/17', '12.00', '32', '421');
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('1',	'09/24/17', '80.00', '32', '369');
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('3',	'09/24/17', '9.80', '33', '221');
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('2',	'09/24/17', '8.00', '33', '123');
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('1',	'09/24/17', '4.00', '33', '145');
+INSERT INTO recieves (Qty_recieved, Datee, P_price, Po_id, P_id)  
+	VALUES('1',	'09/24/17', '7.20', '33', '853');
+
+
+
+
+
+
+
+
+
+
+
