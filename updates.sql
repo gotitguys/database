5093cdadc8b497fd95734c6f40b57248683b4bc7
Tyrannosarous-Esteban <elopez60@csub.edu>
2017-11-28 14:19:49 -0800

    updates sql

--- updates.sql
+++ updates.sql
@@ -1,8 +1,94 @@
-INSERT INTO (Time, S_id, order_num) 
+INSERT INTO updates(Time, S_id, order_num) 
   VALUES('9:14', '72253846', '100000');
-INSERT INTO (Time, S_id, order_num) 
+INSERT INTO updates(Time, S_id, order_num) 
   VALUES('9:14', '72215368', '100001');
-INSERT INTO (Time, S_id, order_num) 
-  VALUES('9:14', '71965222', '1000002');
-INSERT INTO (Time, S_id, order_num) 
-  VALUES('10:30', '72253846', '100003');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES('9:14', '71965222', '100002');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('10:30', '72253846', '100003');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('10:30', '92551366', '100004');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('14:52', '75226415', '100005');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('14:52', '65539751', '100006');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('14:52', '63357910', '100007');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES('9:20', '63462530', '100008');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES('9:20', '72963514', '100009');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES('9:20', '90852369', '100010');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('14:41', '75362547', '1000011');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('14:41', '64008353', '1000012');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES('9:51', '98632536', '1000013');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES('9:51', '64253611', '1000014');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('11:17', '72253512', '100015');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('11:17', '72215365', '100016');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('11:17', '71965579', '100017');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('13:57', '62514325', '100018');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('13:57', '92551659', '100019');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('13:57', '75226321', '100020');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('16:09', '65539692', '100021');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('16:09', '63357326', '100022');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('9:29', '63462642', '100023');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('9:29', '72963359', '100024');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('9:29', '90852387', '100025');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('10:47', '75362215', '100026');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('10:47', '64008369', '100027');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('14:46', '98632248', '100028');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('14:46', '6423368', '100029');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('14:46', '72253147', '100030');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('16:13', '72215258', '100031');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('16:13', '71965369', '100032');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('16:13', '62514123', '100033');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('9:17', '92514123', '100034');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('9:17', '75226789', '100035');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('9:17', '65539123', '100036');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('9:17', '63357159', '100037');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('10:45', '63462357', '100038');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('10:45', '72963158', '100039');
+INSERT INTO updates(Time, S_id, order_num) 
+  VALUES ('10:45', '90852269', '100040');
+
+
+
+
+
+
+
+
+
+
+
+
